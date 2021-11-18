08a9cf315f084ce221c18a6fb1793a670dc28630
Shaw <michele_aliffi@yahoo.it>
2021-11-18 15:23:36 +0100

    End project, start bonus

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -1,10 +1,45 @@
 package org.generation.italy.cicli;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class StampaNumeriDispari {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n;
+		Scanner scanner = new Scanner(System.in);
+		Random randomGenerator = new Random ();
+		
+		System.out.print("Dammi un numero: ");
+		
+		n = scanner.nextInt();
+		int[] numbers = new int [n];
+		
+		for(int i = 0; i < n; i++) {
+			
+			numbers[i] = randomGenerator.nextInt(100);
+			
+			if(numbers[i] % 2 != 0) {
+				System.out.println(numbers[i]);
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			
+			numbers[i] = randomGenerator.nextInt(100);
+			
+			if(numbers[i] % 2 == 0) {
+				System.out.println(numbers[i]);
+			}
+		}
+		
+		for((numbers[i] > 10) && (numbers[i] < 50)) {
+			System.out.println(numbers[i]);
+			sum += i
+		 }
+		 System.out.println("La somma è: " + sum);
+		 
+		scanner.close();
 	}
 
 }
